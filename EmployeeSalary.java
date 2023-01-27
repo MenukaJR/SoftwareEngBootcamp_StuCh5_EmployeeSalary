cadd62be4ac130d0be01cbc0746fa7114df6c0b6
MenukaJR <120049194+MenukaJR@users.noreply.github.com>
2023-01-27 21:34:32 +0530

    Added the main salary calculation mechanisms

--- EmployeeSalary.java
+++ EmployeeSalary.java
@@ -1,5 +1,22 @@
 public class EmployeeSalary {
 
+    public static int calBasicSal(int perDayPayment, int numberOfDays ){
+     return perDayPayment*numberOfDays;
+    }
+
+    public static double calAllowances(int basicSalary,double percentage){
+    return basicSalary*percentage;
+    }
+
+    public static double calEPF(double totalMonthlyIncome, double EPF_Percentage, double employerContributionPercentage){
+        double EPF, employerContribution,total;
+        
+        EPF = totalMonthlyIncome*EPF_Percentage;
+        employerContribution = totalMonthlyIncome*employerContributionPercentage;
+        total = EPF+employerContribution;
+        return total;
+    }
+    
     public static void main(String[] args) {
         
     }
