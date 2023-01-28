9528d834682f5af941075e9dcd7a0da40ea1ab43
MenukaJR <120049194+MenukaJR@users.noreply.github.com>
2023-01-28 14:41:39 +0530

    Added a thread to get employee allowance

--- EmployeeSalary.java
+++ EmployeeSalary.java
@@ -1,23 +1,91 @@
+import java.util.Scanner;
+
+ class calculations{
+
+    private int basicSalary;
+    private double allowance, EPF;
+
+    //percentage used in functions should be converted to decimals [divide by hundred]
+    // Eg: 5% -> 0.05
+    public int getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(int basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public double getAllowance() {
+        return allowance;
+    }
+
+    public void setAllowance(double allowance) {
+        this.allowance = allowance;
+    }
+
+    public double getEPF() {
+        return EPF;
+    }
+
+    public void setEPF(double ePF) {
+        EPF = ePF;
+    }
+
+    public void calBasicSal(int perDayPayment, int numberOfDays ){
+         basicSalary = perDayPayment*numberOfDays;
+       }
+   
+       public double calAllowances(int basicSalary,double percentage){
+        return allowance = basicSalary*percentage;
+       }
+   
+       public void calEPF(double totalMonthlyIncome, double EPF_Percentage, double employerContributionPercentage){
+           double Emp_EPF, employerContribution,total;
+           
+           Emp_EPF = totalMonthlyIncome*EPF_Percentage;
+           employerContribution = totalMonthlyIncome*employerContributionPercentage;
+           total = Emp_EPF+employerContribution;
+           EPF = total;
+       }
+}
+
 public class EmployeeSalary {
 
-    public static int calBasicSal(int perDayPayment, int numberOfDays ){
-     return perDayPayment*numberOfDays;
-    }
-
-    public static double calAllowances(int basicSalary,double percentage){
-    return basicSalary*percentage;
-    }
-
-    public static double calEPF(double totalMonthlyIncome, double EPF_Percentage, double employerContributionPercentage){
-        double EPF, employerContribution,total;
+    synchronized public static void main(String[] args) {
+        int perDayPmnt, days,basicSalary;
+        double allowPercent,allowance;
+        Scanner readInput = new Scanner(System.in);
         
-        EPF = totalMonthlyIncome*EPF_Percentage;
-        employerContribution = totalMonthlyIncome*employerContributionPercentage;
-        total = EPF+employerContribution;
-        return total;
-    }
+        
+        calculations cal = new calculations();
+
+        System.out.println("Enter Per Day Payment rate:  ");
+        perDayPmnt = readInput.nextInt();
+
+        System.out.println("Enter Number of days: ");
+        days = readInput.nextInt();
+
+        cal.calBasicSal(perDayPmnt, days);
+        basicSalary = cal.getBasicSalary();
+
+        allowPercent = 0.05;
+
+        Thread thread1 = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                double allowance;
+
+                allowance = cal.calAllowances(basicSalary,allowPercent);
+                cal.setAllowance(allowance);
+                //System.out.println(cal.getAllowance());
+            }
+
+        });
+
+        thread1.run();
+        System.out.println(cal.getAllowance());
     
-    public static void main(String[] args) {
-        
+
     }
 }
